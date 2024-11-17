052bda4533f91aef9bb235028b6363bab1466693
AdityaKalsi <166589160+AdityaKalsi@users.noreply.github.com>
2024-11-17 23:13:32 +0530

    Update util.py

--- backend/genaihealthcare/App/llm/util.py
+++ backend/genaihealthcare/App/llm/util.py
@@ -1,72 +1,88 @@
-from bson import ObjectId
-from rest_framework.response import Response
-from models import User
-import hashlib
-import random
-import string
-import uuid
-from django.core.exceptions import ObjectDoesNotExist
-from django.utils import timezone
-import environ
-env = environ.Env()
-environ.Env.read_env()
+from peft import PeftModel
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+import yaml
+from sentence_transformers import SentenceTransformer
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core import PromptTemplate
+from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig, TrainingArguments
 
 
-def encrypt_password(password, salt):
-    return hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+def load_config(CONFIG_PATH):
+    with open(CONFIG_PATH, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
 
 
-def generate_salt():
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+def generate_prompt_medical(question, context, answer=None):
+    """Generates a prompt from the given question, context, and answer."""
+    if answer:
+        return f"question: {question} context: {context} answer: {answer} </s>"
+    else:
+        return f"question: {question} context: {context} </s>"
 
 
-def generate_login_token(email, SECRET_KEY, _id):
-    return hashlib.sha256((email + SECRET_KEY + str(_id)).encode()).hexdigest()
+Settings.embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-en-v1.5")
+
+query_wrapper_prompt = PromptTemplate(
+    "Below is an instruction that describes a task. "
+    "Write a response that appropriately completes the request.\n\n"
+    "### Instruction:\n{query_str}\n\n### Response:"
+)
+
+bnb_config = GPTQConfig(bits=4, disable_exllama=True)
+x = "Intel/Mistral-7B-v0.1-int4-inc"
+tokenizer = AutoTokenizer.from_pretrained(x)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = AutoModelForCausalLM.from_pretrained(x,
+                                             quantization_config=bnb_config,
+                                             device_map="auto",
+                                             use_cache=False,
+                                             )
 
 
-def insert_signup(data):
-    name = data.data.get('name')
-    phone_number = data.data.get('phone_number')
-    email = data.data.get('email')
-    password = data.data.get('password')
+llm = HuggingFaceLLM(
+    context_window=2048,
+    max_new_tokens=256,
+    # quantization_config=gptq_config,
+    generate_kwargs={"temperature": 0.25, "do_sample": False},
+    query_wrapper_prompt=query_wrapper_prompt,
+    tokenizer=tokenizer,
+    #    model_name="Intel/neural-chat-7b-v3-1-int4-inc",
+    model=model,
+    device_map="auto",
+    tokenizer_kwargs={"max_length": 2048},
+    # uncomment this if using CUDA to reduce memory usage
+    # model_kwargs={"torch_dtype": torch.float16}
+)
 
-    salt = generate_salt()
-    hashed_password = encrypt_password(password, salt)
-
-    user = User.objects.filter(email=email)
-    if user:
-        return Response({'status': 400, 'message': 'User already exists!'}, status=400)
-
-    try:
-        User.objects.create(name=name, phone_number=phone_number,
-                            email=email, hashed_password=hashed_password, salt=salt)
-    except:
-        return Response({'status': 400, 'message': 'Error during User creation!'}, status=400)
-
-    return Response({'status': 200, 'message': 'User added successfully!'}, status=200)
+Settings.chunk_size = 512
+Settings.llm = llm
 
 
-def user_signin(data):
-    email = data.data.get('email')
-    password = data.data.get('password')
-    user = User.objects.filter(email=email)
-    if not user:
-        return Response({'status': 400, 'message': 'User does not exists!'}, status=400)
+base_model_id = "mistralai/Mistral-7B-v0.1"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
 
-    user = user[0]
-    hashed_password = encrypt_password(password, user.salt)
-    if hashed_password != user.hashed_password:
-        return Response({'status': 400, 'message': 'Invalid password!'}, status=400)
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_id,  # Mistral, same as before
+    quantization_config=bnb_config,  # Same quantization config as before
+    device_map="auto",
+    trust_remote_code=True,
+)
 
-    SECRET_KEY = env('SECRET_KEY')
-    _id = user._id
-    login_token = generate_login_token(
-        email, SECRET_KEY, _id)
+eval_tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    add_bos_token=True,
+    trust_remote_code=True,
+)
 
-    return Response({'status': 200, 'message': 'User logged in successfully!',
-                     'name': user.name,
-                     'phone_number': user.phone_number,
-                     'email': user.email,
-                     'login_token': login_token,
-                     'id': str(_id)
-                     }, status=200)
+ft_model = PeftModel.from_pretrained(base_model, "results/checkpoint-144")
